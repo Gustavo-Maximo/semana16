ec90900bd1f3993c1f1bb263296cb1bbd7fc434f
Gustavo Maximo Da Silva <gmaximodasilva@gmail.com>
2025-11-18 23:42:41 -0300

    Require login for index and fetch users
    Added user authentication and fetched all users for rendering.

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from . import main
-
+from flask_login import login_required, current_user
 
 @main.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    from app.models import User
+    usuarios = User.query.all()
+    return render_template('index.html', usuarios=usuarios)
