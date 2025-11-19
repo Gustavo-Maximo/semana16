259f4d33347d05482de736f9b9d9c2b5f35e1740
Gustavo Maximo Da Silva <gmaximodasilva@gmail.com>
2025-11-18 23:44:27 -0300

    Remove duplicate import of User model

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,10 @@
 from flask import render_template
+from flask_login import login_required
 from . import main
-from flask_login import login_required, current_user
+from app.models import User
 
 @main.route('/')
 @login_required
 def index():
-    from app.models import User
     usuarios = User.query.all()
     return render_template('index.html', usuarios=usuarios)
